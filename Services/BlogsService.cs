a178613d086b666db37207a7b104a011898c419a
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-10-23 14:12:14 -0600

    set up service working on repo

--- Services/BlogsService.cs
+++ Services/BlogsService.cs
@@ -2,6 +2,10 @@ namespace Blogger.Services
 {
   public class BlogsService
   {
-    
+    private readonly BlogsRepository _br;
+      public BlogsService(BlogsRepository br)
+      {
+        _br = br;
+      }
   }
 }
